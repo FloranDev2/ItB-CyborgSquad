264b615c91cb2ab0fe8760881a1aec816d3d49cb
Truelch <floranDev@gmail.com>
2023-05-23 23:27:02 +0200

    WIP scorpion attack

--- scripts/weapons/scorpion_attack.lua
+++ scripts/weapons/scorpion_attack.lua
@@ -11,9 +11,13 @@ truelch_ScorpionAttack = Skill:new{
 	Upgrades = 2,
 	UpgradeCost = {2,2},
 
+	--TC
+	TwoClick = true,
+
 	--Gameplay
+	Range = 1,
 	Damage = 1,
-	ZoneTargeting = ZONE_DIR,
+	--ZoneTargeting = ZONE_DIR,
 
 	--Art
 	--LaunchSound = "/enemy/burnbug_2/attack_launch",
@@ -21,7 +25,10 @@ truelch_ScorpionAttack = Skill:new{
 	--Tip image
 	TipImage = {
 		Unit = Point(2,3),
-	}
+		Target = Point(2,2),
+		Enemy = Point(2,2),
+		Second_Click = Point(3,1),
+	},
 }
 
 Weapon_Texts.truelch_ScorpionAttack_Upgrade1 = "Extended spinneret"
@@ -40,6 +47,40 @@ truelch_ScorpionAttack_AB = truelch_ScorpionAttack:new{
 	Damage = 2,
 }
 
+function truelch_ScorpionAttack:IsControllable(p2)
+
+	if not Board:IsPawnSpace(p2) then
+		return false
+	end
+
+	local pawn = Board:GetPawn(p2)
+		
+	if pawn:IsGuarding() and not pawn:IsBurrower() then
+		return false
+	end
+	
+	if not pawn:IsPowered() then
+		return false
+	end
+	
+	if pawn:IsFrozen() then
+		return false
+	end
+	
+	if pawn:GetType() == "Snowmine1" or pawn:GetType() == "VIP_Truck" then
+		return true
+	end
+	
+	if (pawn:GetMoveSpeed() ~= 0 or pawn:IsGrappled()) and
+		pawn:GetBaseMove() ~= 0 then
+		return true
+	end
+
+	return false
+end
+
+--Old
+--[[
 function truelch_ScorpionAttack:GetTargetArea(point)
 	local ret = PointList()
 	
@@ -50,11 +91,31 @@ function truelch_ScorpionAttack:GetTargetArea(point)
 	
 	return ret
 end
+]]
+
+function truelch_ScorpionAttack:GetTargetArea(point)
+	local ret = PointList()
+	for dir = DIR_START, DIR_END do
+		for range = 1, self.Range do
+			local curr = point + DIR_VECTORS[dir]*range
+			if self:IsControllable(curr) then
+				ret:push_back(curr)
+			end
+		end
+	end
+	
+	return ret
+end
 
 function truelch_ScorpionAttack:GetSkillEffect(p1,p2)
 	local ret = SkillEffect()
-		
-
-
+	local damage = SpaceDamage(p2,0)
+	if Board:IsPawnSpace(p2) then
+		damage.sImageMark = "combat/icons/icon_mind_glow.png"
+	else
+		damage.sImageMark = "combat/icons/icon_mind_off_glow.png"
+	end
+	ret:AddDamage(damage)
 	return ret
 end
+
